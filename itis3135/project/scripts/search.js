e389d740ddf70e19f37a1b392f477e5f9f2d82f8
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-17 07:19:13 -0500

    p

--- itis3135/project/scripts/search.js
+++ itis3135/project/scripts/search.js
@@ -1,34 +1,27 @@
-function search() {
-      // Prevent the form from submitting
-      event.preventDefault();
-
-      // Get the search query from the form
-      var query = document.getElementById("query").value;
-
-      // Set up an AJAX request to the server
-      $.ajax({
+$(document).ready(function() {
+    $.ajax({
         type: "get",
         url: "books.json",
         beforeSend: function() {
-          $("#results").html("Loading...");
+            $("#team").html("Loading...");
         },
         timeout: 10000,
         error: function(xhr, status, error) {
-          alert("Error: " + xhr.status + " - " + error);
+            alert("Error: " + xhr.status + " - " + error);
         },
         dataType: "json",
         success: function(data) {
-          $("#results").html("");
-          $.each(data.books, function(key, value) {
-            if (value.title.toLowerCase().includes(query.toLowerCase()) || value.author.toLowerCase().includes(query.toLowerCase())) {
-              $("#results").append(
-                "<img src='" + value.image + "' alt='Image of "+value.title +"'></img>" +
-                "<h2>" + "" + value.title + "</h2>" +
-                "<h3>" + "" + value.author + "</h3>" +
-                "<p>" + "" + value.description + "</p>"
-              );
-            }
-          });
+            $("#books").html("");
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $("#books").append(
+
+                        "<h2>" + "" + value.title + "</h2>" +
+                        "<h3>" + "" + value.author + "</h3>" +
+                        "<p>" + "" + value.description + "</p>"
+                    );
+                });
+            });
         }
-      });
-    }
+    });
+  });
