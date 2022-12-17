03a1f3866e766f1f55e58791f0ea9285dcfc18a7
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-17 01:08:21 -0500

    p

---
+++ itis3135/Activity11/Activity11.js
@@ -0,0 +1,9 @@
+// <!-- jQuery call to the accordion() method. -->
+// jQuery call to the accordion() method.
+$(document).ready(function() {
+    $("#accordion").accordion({
+        event: "click",
+        heightStyle: "content",
+        collapsible: true
+    })
+});
