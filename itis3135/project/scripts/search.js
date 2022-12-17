62107aa3879973cf523dadc79a202257f7fc271e
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-17 08:05:28 -0500

    p

--- itis3135/project/scripts/search.js
+++ itis3135/project/scripts/search.js
@@ -1,34 +1,8 @@
 
-    function search() {
-      // Prevent the form from submitting
-      event.preventDefault();
-
-      // Get the search query from the form
-      var query = document.getElementById("query").value;
-
-      // Set up an AJAX request to the server
-      $.ajax({
-        type: "get",
-        url: "books.json",
-        beforeSend: function() {
-          $("#books").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(xhr, status, error) {
-          alert("Error: " + xhr.status + " - " + error);
-        },
-        dataType: "json",
-        success: function(data) {
-          $("#books").html("");
-          $.each(data, function(key, value) {
-            if (value.title.toLowerCase().includes(query.toLowerCase()) || value.author.toLowerCase().includes(query.toLowerCase())) {
-              $("#books").append(
-                "<h2>" + "" + value.title + "</h2>" +
-                "<h3>" + "" + value.author + "</h3>" +
-                "<p>" + "" + value.description + "</p>"
-              );
-            }
-          });
-        }
-      });
-    }
+    var search = new XMLHttpRequest();
+    search.open('Get', 'https://mavedave6.github.io/itis3135/project/books.json');
+    search.onload = function(){
+      var myData = JSON.parse(search.responseText);
+      console.log(myData[2]);
+    };
+    search.send();
