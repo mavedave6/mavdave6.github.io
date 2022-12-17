8d64c407852fb830918d0ed7d2f0c4c96cc770ec
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-17 07:37:55 -0500

    p

--- itis3135/project/scripts/search.js
+++ itis3135/project/scripts/search.js
@@ -1,27 +1,34 @@
-$(document).ready(function() {
-    $.ajax({
+
+    function search() {
+      // Prevent the form from submitting
+      event.preventDefault();
+
+      // Get the search query from the form
+      var query = document.getElementById("query").value;
+
+      // Set up an AJAX request to the server
+      $.ajax({
         type: "get",
         url: "books.json",
         beforeSend: function() {
-            $("#team").html("Loading...");
+          $("#books").html("Loading...");
         },
         timeout: 10000,
         error: function(xhr, status, error) {
-            alert("Error: " + xhr.status + " - " + error);
+          alert("Error: " + xhr.status + " - " + error);
         },
         dataType: "json",
         success: function(data) {
-            $("#books").html("");
-            $.each(data, function() {
-                $.each(this, function(key, value) {
-                    $("#books").append(
-
-                        "<h2>" + "" + value.title + "</h2>" +
-                        "<h3>" + "" + value.author + "</h3>" +
-                        "<p>" + "" + value.description + "</p>"
-                    );
-                });
-            });
+          $("#books").html("");
+          $.each(data.books, function(key, value) {
+            if (value.title.toLowerCase().includes(query.toLowerCase()) || value.author.toLowerCase().includes(query.toLowerCase())) {
+              $("#books").append(
+                "<h2>" + "" + value.title + "</h2>" +
+                "<h3>" + "" + value.author + "</h3>" +
+                "<p>" + "" + value.description + "</p>"
+              );
+            }
+          });
         }
-    });
-  });
+      });
+    }
