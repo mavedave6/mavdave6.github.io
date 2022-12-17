5a1b8a35bcd31e9e69a9fb5545f073f82c773125
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-16 20:08:25 -0500

    p

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,28 +1,37 @@
-let Index = 0;
-    showSlides(Index);
+window.onload = function(){
 
-    function plusSlides(n) {
-      showSlides(Index += n);
-    }
+}
 
-    function currentSlide(n) {
-      showSlides(Index = n);
-    }
+var fadeSpeed = 2000;
+var container = $('#images');
+var curIndex = 2;
+var clickedLast = "right";
 
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("demo");
-      let captionT = document.getElementById("caption");
-      if (n > slides.length) {Index = 1}
-      if (n < 1) {Index = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[Index-1].style.display = "block";
-      dots[Index-1].className += " active";
-      captionT.innerHTML = dots[Index-1].alt;
-    }
+$(document).ready(function() {
+    container + $('#image1').fadeIn(fadeSpeed);
+    $('#right-arrow').click(function(){
+        if(curIndex > 0 && curIndex <= 6){
+            clickedLast = "right";
+            container + $('#image' + (curIndex - 1)).fadeOut(fadeSpeed);
+            container + $('#image' + curIndex).fadeIn(fadeSpeed);
+            if(curIndex != 6){
+                curIndex++;
+            }
+        }
+    })
+    $('#left-arrow').click(function(){
+        clickedLast = "left";
+        if(curIndex > 0 && curIndex <= 6){
+            if(clickedLast == "right"){
+                container + $('#image' + (curIndex - 1)).fadeOut(fadeSpeed);
+            }
+            container + $('#image' + (curIndex + 1)).fadeOut(fadeSpeed);
+            container + $('#image' + curIndex).fadeIn(fadeSpeed);
+            if(curIndex != 1){
+                curIndex--;
+            }
+            clickedLast = "left";
+        }
+    })
+
+});
