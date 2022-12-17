892850b0281f20031028658b92ff6b57048b113c
mavedave6 <abasiubongudoeyop@gmail.com>
2022-12-17 03:25:49 -0500

    Update Udoeyop_Activity13.js

--- itis3135/Activity13/Udoeyop_Activity13.js
+++ itis3135/Activity13/Udoeyop_Activity13.js
@@ -1,26 +1,33 @@
-$(document).ready(function() {
-  $.ajax({
-    type: "get",
-    url: "team.json",
-    beforeSend: function() {
-        $("#team").html("Loading...");
-    },
-    timeout: 10000,
-    error: function(xhr, status, error) {
-        alert("Error: " + xhr.status + " - " + error);
-    },
-    dataType: "json",
-    success: function(data) {
-        $("#team").html("");
-        for (let i = 0; i < data.management.length; i++) {
-            let management = data.management[i];
-            $("#team").append(
-                "<h3>" + management.name + "</h3>" +
-                management.title + "<br>" +
-                management.bio + "<br>"
+$$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "team.json",
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#team").html("");
+            $.each(data, function()
+                {
+                    $.each(this, function(key, value){
+                        $("#team").append('<h3>' + value.name + '</h3>');
+                        $("#team").append('<p>' + value.title + '</p>');
+                        $("#team").append('<p>' + value.bio + '</p>')
+                    })
+                }
             );
+            $(data).find("management").children().each(function() {
+                var xmlDoc = $(this);
+                $("#team").append
+                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
+                          xmlDoc.find("title").text() + "<br>" +
+                          xmlDoc.find("bio").text() + "<br>");
+            });
         }
-    }
-});
-
+    });
 });
